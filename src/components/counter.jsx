9f18d489d3e4d83f79c327407fc0d044369d4b57
aeonincode <khashayar.sasannia@hotmail.com>
2020-05-29 12:39:25 +0200

    attributes children component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,13 +1,51 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
+  state = {
+    count: 0,
+  };
+
+  // Apply style to specific element
+  // styles = {
+  //   fontSize: 10,
+  //   fontWeight: 'bold',
+  // };
+
+  // render() {
+  //   return (
+  //     <div>
+  //       <span style={this.styles} className='badge badge-primary m-2'>
+  //         {this.formatCount()}
+  //       </span>
+  //       <button className='btn btn-secondary btn-sm'>Increment</button>
+  //     </div>
+  //   );
+  // }
+
+  // inline style
+  // render() {
+  //   return (
+  //     <div>
+  //       <span style={{ fontSize: 30 }} className='badge badge-primary m-2'>
+  //         {this.formatCount()}
+  //       </span>
+  //       <button className='btn btn-secondary btn-sm'>Increment</button>
+  //     </div>
+  //   );
+  // }
+
   render() {
     return (
-      <React.Fragment>
-        <h1>Hello World</h1>
-        <button>Increment</button>
-      </React.Fragment>
+      <div>
+        <span className='badge badge-primary m-2'>{this.formatCount()}</span>
+        <button className='btn btn-secondary btn-sm'>Increment</button>
+      </div>
     );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? 'Zero' : count;
   }
 }
 
